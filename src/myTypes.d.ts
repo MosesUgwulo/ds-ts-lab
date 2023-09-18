e1435b443ff79d6266d00806ff5559e4b4574116
Moses Ugwulo <74902008+MosesUgwulo@users.noreply.github.com>
2023-09-18 20:15:18 +0100

    Define types using Utility types.

--- src/myTypes.d.ts
+++ src/myTypes.d.ts
@@ -44,3 +44,43 @@ export type BuddyList = {
   administrator: Administrator;
   members: Buddy[];
 };
+
+export type FriendPartial = Partial<Friend>
+// Type for gaining access to an event, e.g. concert
+export type EventPass = Omit<Colleague, "contact"> & {
+  passCode : number;
+}
+// Make person's properties immutable.
+export type SecureFriendContact = Readonly<Pick<Friend,"name" | "phone" > > 
+
+function generateEventPass(colleague: Colleague): EventPass {
+  const passCode = Math.round(Math.random() * (1000 - 1) + 1);
+  return {
+    name: colleague.name,
+    department: colleague.department,
+    passCode: passCode,
+  };
+}
+console.log(generateEventPass(colleagues.current[0]));
+
+//The incomplete function below finds the intersection of a friends array and a colleagues array, based on matching the name property. It returns an array of objects with a mix of properties from the Friend and Colleagues types, as follows:
+function intersection(
+  friends: Friend[],
+  colleagues: Colleague[]
+): (Friend | Colleague)[] {
+  let result = [];
+  friends.reduce((res, friend) => {
+    const colleague = colleagues.find((col) => col.name === friend.name);
+    if (colleague) {
+      // Colleague is also a Friend
+      res.push({ ...friend, ...colleague });
+    } else {
+      // Colleague is not a Friend
+      res.push(friend);
+    }
+    return res;
+  }, result);
+  return result;
+}
+
+console.log(intersection(friends, colleagues.current));
