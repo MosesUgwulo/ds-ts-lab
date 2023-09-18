b1f0b17037c4f19507c86f14343283853127f276
Moses Ugwulo <74902008+MosesUgwulo@users.noreply.github.com>
2023-09-18 11:39:37 +0100

    Added type-safety to colleague data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -19,8 +19,17 @@ const friend1:Friend = {
   const friends:Friend[] = [friend1, friend2];
   console.log(friends[1]);
   
+  interface Colleague {
+    name: string;
+    department: string;
+    contact: {
+      email: string;
+      extension: number;
+    };
+  }
+
   //   -------------------
-  const colleague1 = {
+  const colleague1:Colleague = {
     name: "Ralph Graham",
     department: "Engineering",
     contact: {
@@ -29,7 +38,7 @@ const friend1:Friend = {
     },
   };
   
-  const colleague2 = {
+  const colleague2:Colleague = {
     name: "Patti Burke",
     department: "Finance",
     contact: {
@@ -38,7 +47,7 @@ const friend1:Friend = {
     },
   };
   
-  const colleague3 = {
+  const colleague3:Colleague = {
     name: "Dean Sullivan",
     department: "HR",
     contact: {
