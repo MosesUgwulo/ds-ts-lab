d5ccee7ec7fb7757a3ba1edb5cac1b9581ca881a
Moses Ugwulo <74902008+MosesUgwulo@users.noreply.github.com>
2023-09-18 11:41:28 +0100

    Complete type-safety to colleagues data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -8,13 +8,13 @@ const friend1:Friend = {
     name: "Paul Fleming",
     phone: "087-12345",
     age: 25,
-  };
+};
   
-  const friend2:Friend = {
-    name: "Jane Costello",
-    phone: "086--12345",
-    age: 31,
-  };
+const friend2:Friend = {
+  name: "Jane Costello",
+  phone: "086--12345",
+  age: 31,
+};
   
   const friends:Friend[] = [friend1, friend2];
   console.log(friends[1]);
@@ -55,7 +55,13 @@ const friend1:Friend = {
       extension: 125,
     },
   };
-  const colleagues = {
+
+  interface ColleagueHistory {
+    current: Colleague[],
+    former: Colleague[]
+  }
+
+  const colleagues : ColleagueHistory = {
     current: [colleague1, colleague2, colleague3],
     former: [],
   };
